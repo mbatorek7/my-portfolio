09f9660359ce19151710cec2289d258a28e44f8f
mbatorek7 <maegan.batorek@valpo.edu>
2025-07-19 15:34:15 -0500

    matrix animation added and components are layering properly on top

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Features, Footer, Hero, Navbar } from './components'
+import { Features, Footer, Hero, MatrixRainingCode, Navbar } from './components'
 
 const App = () => {
   return (
     <div className='min-h-screen overflow-hidden'>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Footer />
+
+      <MatrixRainingCode className='absolute inset-0' />
+
+      <div className='flex flex-col'>
+        <Navbar />
+        <Hero />
+        <Features />
+        <Footer />
+      </div>
+
     </div>
   )
 }
--- src/components/MatrixRainingCode.jsx
+++ src/components/MatrixRainingCode.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const MatrixRainingCode = () => {
-  return (
-    <div>
-      Matrix Code
-    </div>
-  )
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        let width = (canvas.width = window.innerWidth);
+        let height = (canvas.height = window.innerHeight);
+
+        ctx.fillStyle = "black"
+        ctx.fillRect(0, 0, width, height)
+
+        let columns = Math.floor(width / 20);
+        const characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+        const charArray = characters.split("");
+        let drops = [];
+
+        for (let i = 0; i < columns; i++) {
+            drops[i] = 1;
+        }
+
+        let frameRate = 25;
+        let lastFrameTime = Date.now();
+
+        const draw = () => {
+            ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+            ctx.fillRect(0, 0, width, height);
+
+            ctx.fillStyle = "#0f0";
+            ctx.font = "15px monospace";
+
+            for (let i = 0; i < drops.length; i++) {
+                const text = charArray[Math.floor(Math.random() * charArray.length)]
+                ctx.fillText(text, i * 20, drops[i] * 20);
+
+                if (drops[i] * 20 > height && Math.random() > 0.975) {
+                    drops[i] = 0;
+                }
+
+                drops[i]++;
+            }
+        }
+
+        const animate = () => {
+            const currentTime = Date.now();
+            const elapsedTime = currentTime - lastFrameTime;
+
+            if (elapsedTime > 1000 / frameRate) {
+                draw();
+                lastFrameTime = currentTime;
+            }
+            requestAnimationFrame(animate)
+        }
+
+        animate();
+
+        const handleResize = () => {
+            width = canvas.width = window.innerWidth;
+            height = canvas.height = window.innerHeight;
+            columns = Math.floor(width / 20)
+            drops = []
+            for (let i = 0; i < columns; i++) {
+                drops[i] = 1;
+            }
+        }
+
+        const isMobileDevice = /Mobi/i.test(window.navigator.userAgent)
+        if (!isMobileDevice) {
+            window.addEventListener('resize', handleResize)
+        }
+
+        return () => {
+            if (!isMobileDevice) {
+                window.addEventListener('resize', handleResize)
+            }
+        }
+    }, [])
+    return <canvas className='fixed top-0 left-0 w-full h-full z-[-1]' ref={canvasRef}></canvas>;
 }
 
 export default MatrixRainingCode
