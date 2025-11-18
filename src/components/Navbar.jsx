83938905f71cd40254985186e15ded6209cc1fc0
mbatorek7 <maegan.batorek@valpo.edu>
2025-11-17 21:12:50 -0600

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { Link } from 'react-scroll'
+import { Link } from 'react-router-dom'
 
 const Navbar = ({ onNameClick }) => {
   return (
